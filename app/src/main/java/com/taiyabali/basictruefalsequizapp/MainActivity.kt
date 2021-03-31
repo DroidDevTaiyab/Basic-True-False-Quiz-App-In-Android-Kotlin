6339605c92a1e92d559cd96eb1393e466c02ceb3
ali <taiyaba@siliconasiaworks.com>
2021-04-01 01:56:38 +0530

    init commit

---
+++ app/src/main/java/com/taiyabali/basictruefalsequizapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.taiyabali.basictruefalsequizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by Techpass Master on 1-April-21.
+ * Website - https://techpassmaster.com/
+ * Email id - hello@techpassmaster.com
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
